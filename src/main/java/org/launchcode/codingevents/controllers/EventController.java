ea0daee46bae7b473e56f29a35034b1f5bd57356
Ingrid Barrett <barrett92@yahoo.com>
2022-02-14 18:25:33 -0500

    first commit of ch. 11 exercises - not completed

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.print.attribute.standard.Severity;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -28,14 +30,21 @@ public class EventController {
 
     @GetMapping("create")
     public String displayCreateEventForm(Model model) {
-        model.addAttribute("title", "Create Event");
+    // Let’s make our events list a HashMap! This enables us to add descriptions to our events.
+    // Fill your events HashMap with the names and descriptions of 3 coding events around town.
+//        model.addAttribute("title", "Create Event");
+        HashMap<String, String> events = new HashMap<>();
+        events.put("Hour of Code", "Take and hour to code and learn.");
+        events.put("Strange Loop", "Learn to code a variety of loops.");
+        events.put("CES", "Come and mingle.");
+    model.addAttribute("events", events);
         return "events/create";
     }
 
-    @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName) {
-        events.add(eventName);
-        return "redirect:";
-    }
+//    @PostMapping("create")
+//    public String processCreateEventForm(@RequestParam String eventName) {
+//        events.add(eventName);
+//        return "redirect:";
+//    }
 
 }
